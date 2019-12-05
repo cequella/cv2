77fde1e8870d55d88305f0e492516a5e7cd889e9
PedroOliveira <pedro.oliveira@wdatecnologia.com.br>
2019-12-05 15:26:54 -0300

    Saving progress

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,7 +3,8 @@ import {
   OnInit,
   AfterViewInit,
   ViewChild,
-  ElementRef
+  ElementRef,
+  HostListener
 } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -18,6 +19,7 @@ export class Tuple {
 }
 export class Occurrence {
   name: string;
+  beachs: string;
   coord: number[];
   weight: string;
   dead: Tuple[];
@@ -33,6 +35,7 @@ export class HomePageComponent implements OnInit, AfterViewInit {
   @ViewChild('featureScreen', {static: false}) featureScreen: ElementRef;
 
   map: any;
+  image: any;
 
   data: Occurrence[];
 
@@ -53,8 +56,8 @@ export class HomePageComponent implements OnInit, AfterViewInit {
     mapboxgl.accessToken = environment.apiKey;
     this.map = new mapboxgl.Map({
       container: 'map',
-      //style: 'mapbox://styles/mapbox/streets-v9',
-      style: 'mapbox://styles/jfilipedias/ck3nc19d25gre1cqpyqpdpm4r',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      //style: 'mapbox://styles/jfilipedias/ck3nc19d25gre1cqpyqpdpm4r',
       center: [-38.7499, -3.6196],
       zoom: 7,
       minZoom: 0,
@@ -73,30 +76,39 @@ export class HomePageComponent implements OnInit, AfterViewInit {
   }
 
   onContentLoad() {
-    //let link = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png';
-    /*
-    let min = 1;
-    let max = 5;
-
-    let temp = this.data.map(
-      x => Math.floor(Math.random() * (max - min)) + min
-    );
-    */
-
-    let link = 'assets/Mancha 1.png';
-    this.map.loadImage(link,
-      (error, image) => {
-        if (error) throw error;
-        this.createOilStain(image, "target")
-      });
+    if(this.image == null) {
+      let link = 'assets/Mancha 1.png';
+      this.map.loadImage(link,
+        (error, image) => {
+          if (error) throw error;
+          this.image = image;
+          this.map.addImage("target", this.image);
+          this.createOilStain()
+        });
+    } else {
+      this.createOilStain()
+    }
   }
 
-  private createOilStain(image, layerName) {
+  @HostListener('click', ['$event']) onMarkerClick(bla) {
+    if(bla.target.id == 'clean') {
+      let beachName = bla.target.name;
+      let marker = this.data.filter(x => x.name === beachName)[0];
+
+      debugger;
+      this.map.removeLayer("target");
+
+      debugger;
+      this.data = this.data.filter(x => x.name != beachName);
+      this.onContentLoad();
+    }
+  }
+
+  private createOilStain() {
     var temp = this.data.map(target => this.mapTargetToIcon(target));
 
-    this.map.addImage(layerName, image);
     this.map.addLayer({
-      id: layerName,
+      id: "target",
       type: "symbol",
       source: {
         type: "geojson",
@@ -106,15 +118,15 @@ export class HomePageComponent implements OnInit, AfterViewInit {
         }
       },
       layout: {
-        "icon-image": layerName,
+        "icon-image": "target",
         "icon-size": 0.1,
         "icon-allow-overlap": true
       }
     });
 
-    this.map.on('click', layerName,
+    this.map.on('click', "target",
       (event) => {
-        let targetInfo = this.map.queryRenderedFeatures(event.point, { layers: [layerName] });
+        let targetInfo = this.map.queryRenderedFeatures(event.point, { layers: ["target"] });
         this.showOilContent(targetInfo[0].properties.marker);
     });
   }
@@ -135,15 +147,13 @@ export class HomePageComponent implements OnInit, AfterViewInit {
   private showOilContent(markerData) {
     let occurrence = <Occurrence>JSON.parse(markerData); // Gambiarra
 
-    console.log(occurrence);
-
     var comparation = "";
     if(occurrence.comparation.length > 0) {
-      comparation = "<p><strong>Que equivalem a:</strong></p><ul>";
+      comparation = "<p><strong>Que equivalem a:</strong><br/>";
       comparation += occurrence.comparation
-                                .map(x => "<il>"+x.amount+" "+x.kind+"</il>")
+                                .map(x => `${x.amount} ${x.kind}<br/>`)
                                 .reduce((o,c) => o+c);
-      comparation += "</ul>";
+      comparation += "</p>";
     }
 
     new mapboxgl.Popup({
@@ -151,9 +161,10 @@ export class HomePageComponent implements OnInit, AfterViewInit {
     }).setLngLat(occurrence.coord)
       .setHTML(`
         <h1>${occurrence.name}</h1>
-        <p><strong>Quantidade:</strong> ${occurrence.weight}</p>
+        <p><strong>Praias afetadas:</strong><br/> ${occurrence.beachs}</p>
+        <p><strong>Quantidade:</strong><br/> ${occurrence.weight}</p>
         ${comparation}
-        <button>Limpar</button>
+        <button id="clean" name="${occurrence.name}">Limpar</button>
         `)
       .addTo(this.map);
   }
